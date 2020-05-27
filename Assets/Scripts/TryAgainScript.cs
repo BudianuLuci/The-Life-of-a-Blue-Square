191058da927325e7d8a483596456f0c0427b4afe
BudianuLuci <39630762+BudianuLuci@users.noreply.github.com>
2020-05-28 02:51:12 +0300

    Added Scenes
    TRY AGAIN BUTTON DOESN'T WORK!

--- Assets/Scripts/TryAgainScript.cs
+++ Assets/Scripts/TryAgainScript.cs
@@ -1,14 +1,12 @@
-﻿
-using UnityEngine;
+﻿ using UnityEngine;
 
 public class TryAgainScript : MonoBehaviour
 {
     public GameManager gameManager;
     public float restartDelay = 0.5f; //1f
-
-
     public void RestartGame()
     {
+        Debug.Log("RESTART GAME");
         gameManager.Invoke("Restart", restartDelay);
     }
 }
