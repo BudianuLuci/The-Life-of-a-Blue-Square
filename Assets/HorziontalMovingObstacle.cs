0ba077c9f2f56ad65b561ef82644247b5d06536c
BudianuLuci <39630762+BudianuLuci@users.noreply.github.com>
2020-05-27 18:39:54 +0300

    Added HorizontalMovingObstacle
    Single wall moving left/right

---
+++ Assets/HorziontalMovingObstacle.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class HorziontalMovingObstacle : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Vector3 initial;
+    public Transform posinitial;
+    void Start()
+    {
+        rb.AddForce(0, 0, 500000 * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+
+        if (collisionInfo.collider.name == "WallRight")
+        {
+            rb.AddForce(0, 0, 500000 * Time.deltaTime);
+        }
+        if (collisionInfo.collider.name == "WallLeft")
+        {
+            rb.AddForce(0, 0, -500000 * Time.deltaTime);
+        }
+        //rb.AddForce(0, 0, -500000 * Time.deltaTime);
+        //if (collisionInfo.collider.tag == "Ground")
+        //{
+
+        //  transform.position = initial;
+
+        //}
+    }
+}
--- Assets/Scripts/MovingObs.cs
+++ Assets/Scripts/MovingObs.cs
@@ -5,7 +5,6 @@ public class MovingObs : MonoBehaviour
 {
     public Rigidbody rb;
     public Vector3 initial;
-    public Vector3 rot;
 
     void OnCollisionEnter(Collision collisionInfo)
     {
@@ -13,8 +12,6 @@ public class MovingObs : MonoBehaviour
         {
             //movement.enabled = false;
             transform.position = initial;
-
-            Debug.Log("obs hit the ground!");
         }
     }
 }
