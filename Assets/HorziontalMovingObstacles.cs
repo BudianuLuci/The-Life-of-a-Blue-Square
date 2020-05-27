77717349277ad61137e87d9c0ddeda8b164cd636
BudianuLuci <39630762+BudianuLuci@users.noreply.github.com>
2020-05-27 19:03:59 +0300

    Added Horizontal Moving Obstacles
    2 Obstacles hitting each other and the margins

---
+++ Assets/HorziontalMovingObstacles.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+
+public class HorziontalMovingObstacles : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform posinitial;
+    void Start()
+    {   
+        if (posinitial.position.z > 0)
+        {
+            rb.AddForce(0, 0, -500000 * Time.deltaTime);
+        }
+        else
+        {
+            rb.AddForce(0, 0, 500000 * Time.deltaTime);
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+
+        if (collisionInfo.collider.name == "WallRight")
+        {
+            rb.AddForce(0, 0, 500000 * Time.deltaTime);
+        }
+        if (collisionInfo.collider.name == "WallLeft")
+        {
+            rb.AddForce(0, 0, -500000 * Time.deltaTime);
+        }
+        if (collisionInfo.collider.name == "HMovingObstacleLeft")
+        {
+            rb.AddForce(0, 0, -500000 * Time.deltaTime);
+            Debug.Log("HIT THE LEFT OBSTACLE");
+        }
+        if (collisionInfo.collider.name == "HMovingObstacleRight")
+        {
+            rb.AddForce(0, 0, 500000 * Time.deltaTime);
+        }
+       
+        //rb.AddForce(0, 0, -500000 * Time.deltaTime);
+        //if (collisionInfo.collider.tag == "Ground")
+        //{
+
+        //  transform.position = initial;
+
+        //}
+    }
+}
